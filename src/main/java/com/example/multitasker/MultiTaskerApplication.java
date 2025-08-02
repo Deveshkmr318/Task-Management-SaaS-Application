cee84c089e0f3c99cbdacfbac701ccdd518df7ec
Devesh Kumar Singh <deveshkmr318@gmail.com>
2025-08-02 18:26:15 +0530

    Fresh Project

---
+++ src/main/java/com/example/multitasker/MultiTaskerApplication.java
@@ -0,0 +1,13 @@
+package com.example.multitasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTaskerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTaskerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multitasker/MultiTaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multitasker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTaskerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
