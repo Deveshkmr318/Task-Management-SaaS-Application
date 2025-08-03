062c4d98e5037ee5ff512e0c9c99828789100ca4
Devesh Kumar Singh <deveshkmr318@gmail.com>
2025-08-03 21:38:51 +0530

    Fresh Project

--- src/main/java/com/example/multitasker/controller/UserController.java
+++ src/main/java/com/example/multitasker/controller/UserController.java
@@ -7,6 +7,8 @@ import com.example.multitasker.exception.UserNotFoundException;
 import com.example.multitasker.model.User;
 import com.example.multitasker.response.ResponseHandler;
 import com.example.multitasker.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,8 @@ import java.util.Optional;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     private final UserService userService;
 
     @Autowired
@@ -28,11 +32,17 @@ public class UserController {
     @PostMapping("/")
     public ResponseEntity<?> createUser(@RequestBody UserRequest userRequest){
 
+        logger.info("Start UserController : createUser() method ...");
+
         User user = UserMapper.toEntity(userRequest);
 
         User savedUser = userService.createUser(user);
 
         UserResponse response = UserMapper.toResponse(savedUser);
+
+        logger.debug("Saved user id: {}", savedUser.getId());
+
+        logger.info("End UserController : createUser() method ...");
 
         return ResponseHandler.success("User created successfully !!!", response);
     }
@@ -40,15 +50,21 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<?> getUserById(@PathVariable Long id) throws UserNotFoundException {
 
+        logger.info("Start UserController : getUserById() method ...");
+
         Optional<User> user = userService.getUserById(id);
 
         if(user.isPresent()) {
 
             UserResponse response = UserMapper.toResponse(user.get());
 
+            logger.info("End UserController : getUserById() method ...");
+
             return ResponseHandler.success("User Found successfully !!!", response);
 
         }
+
+        logger.error("End UserController : getUserById() method ...");
 
         return ResponseHandler.error("User is Not Found !!!", HttpStatus.NOT_FOUND);
 
--- src/main/java/com/example/multitasker/dto/mapper/UserMapper.java
+++ src/main/java/com/example/multitasker/dto/mapper/UserMapper.java
@@ -1,31 +1,54 @@
 package com.example.multitasker.dto.mapper;
 
+import com.example.multitasker.controller.UserController;
 import com.example.multitasker.dto.UserRequest;
 import com.example.multitasker.dto.UserResponse;
+import com.example.multitasker.model.Tenant;
 import com.example.multitasker.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UserMapper {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserMapper.class);
+
     public static User toEntity(UserRequest userRequest){
+
+        logger.info("Start UserMapper : toEntity() method ...");
 
         User user = new User();
 
         user.setUserName(userRequest.getUserName());
         user.setEmail(userRequest.getEmail());
         user.setRole(userRequest.getRole());
+        user.setPassword(userRequest.getPassword());
+//        user.setTenant(new Tenant());
+
+        logger.debug("User user -> " + user);
+        logger.debug("UserRequest userRequest -> " + userRequest);
+
+        logger.info("End UserMapper : toEntity() method ...");
 
         return user;
     }
 
     public static UserResponse toResponse(User user){
 
+        logger.info("Start UserMapper : toResponse() method ...");
+
         UserResponse userResponse = new UserResponse();
 
+        userResponse.setId(user.getId());
         userResponse.setEmail(user.getEmail());
         userResponse.setPassword(user.getPassword());
         userResponse.setRole(user.getRole());
         userResponse.setUserName(user.getUserName());
-        userResponse.setTenantId(user.getTenant().getId());
+        userResponse.setTenantId(user.getTenant() != null ? user.getTenant().getId() : null);
+
+        logger.debug("User user -> " + user);
+        logger.debug("UserResponse userResponse -> " + userResponse);
+
+        logger.info("End UserMapper : toResponse() method ...");
 
         return userResponse;
     }
